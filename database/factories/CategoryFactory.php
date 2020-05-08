921c548e797b4212037de23ec74d60929ebc795e
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2020-05-08 00:58:30 -0700

    factory and seeds

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,11 @@
 
 use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $name = $faker->sentence,
+        'slug' => Str::slug($name),
     ];
 });
