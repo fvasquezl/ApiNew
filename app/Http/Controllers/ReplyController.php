07cc76b7e385426a1ceb9947c0718480d739d7d8
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2020-05-08 14:43:19 -0700

    reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,69 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Question;
 use App\Model\Reply;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReplyController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param  Question  $question
+     * @return void
      */
-    public function index()
+    public function index(Question $question)
     {
-        //
+        return $question->replies;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      *
+     * @param  Question  $question
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Application|ResponseFactory|\Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Question $question, Request $request)
     {
-        //
+        $reply = $question->replies()->create($request->all());
+
+        return response(['reply'=> $reply], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
+     * @param  Question  $question
+     * @param  Reply  $reply
+     * @return Reply
      */
-    public function show(Reply $reply)
+    public function show(Question $question, Reply $reply)
     {
-        //
+        return $reply;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reply $reply)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Reply  $reply
+     * @param  Reply  $reply
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Reply $reply)
@@ -75,11 +65,15 @@ class ReplyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Reply  $reply
+     * @param  Question  $question
+     * @param  Reply  $reply
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
-    public function destroy(Reply $reply)
+    public function destroy(Question $question, Reply $reply)
     {
-        //
+        $reply->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
